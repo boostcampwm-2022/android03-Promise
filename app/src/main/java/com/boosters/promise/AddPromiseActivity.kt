151680c963241f1b2b85891f44443c866c4c35ce
hoho-97 <cksgh5652@gmail.com>
2022-11-14 20:34:23 +0900

    refactor: 날짜 선택 MaterialDatePicker로 변경 #6

--- app/src/main/java/com/boosters/promise/AddPromiseActivity.kt
+++ app/src/main/java/com/boosters/promise/AddPromiseActivity.kt
@@ -1,11 +1,12 @@
 package com.boosters.promise
 
-import android.app.DatePickerDialog
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.boosters.promise.databinding.ActivityAddPromiseBinding
+import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.datepicker.MaterialDatePicker.INPUT_MODE_CALENDAR
 import com.google.android.material.timepicker.MaterialTimePicker
 import com.google.android.material.timepicker.MaterialTimePicker.INPUT_MODE_CLOCK
 import com.google.android.material.timepicker.TimeFormat
@@ -21,41 +22,54 @@ class AddPromiseActivity : AppCompatActivity() {
 
         _binding = DataBindingUtil.setContentView(this, R.layout.activity_add_promise)
 
-        val calendar = Calendar.getInstance()
+        val calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"))
         binding.buttonAddPromiseSelectDate.setOnClickListener(selectDateListener(calendar))
         binding.buttonAddPromiseSelectTime.setOnClickListener(selectTimeListener(calendar))
-
-
     }
 
     private fun selectDateListener(cal: Calendar) = View.OnClickListener {
-        val dateSetListener = DatePickerDialog.OnDateSetListener { _, year, month, day ->
-            binding.buttonAddPromiseSelectDate.text = "%d/%d/%d".format(year, month + 1, day)
-        }
+        val today = MaterialDatePicker.todayInUtcMilliseconds()
+        val datePicker = MaterialDatePicker.Builder.datePicker()
+            .setSelection(today)
+            .setInputMode(INPUT_MODE_CALENDAR)
+            .setTitleText(getString(R.string.title_datepicker))
+            .build()
 
-        DatePickerDialog(
-            this,
-            dateSetListener,
-            cal.get(Calendar.YEAR),
-            cal.get(Calendar.MONTH),
-            cal.get(Calendar.DAY_OF_MONTH)
-        ).show()
+        datePicker.show(supportFragmentManager, DATEPICKER_TAG)
+        datePicker.addOnPositiveButtonClickListener {
+            cal.timeInMillis = datePicker.selection ?: today
+            binding.buttonAddPromiseSelectDate.text =
+                getString(R.string.date_format).format(
+                    cal.get(Calendar.YEAR),
+                    cal.get(Calendar.MONTH) + 1,
+                    cal.get(Calendar.DAY_OF_MONTH)
+                )
+        }
     }
 
     private fun selectTimeListener(cal: Calendar) = View.OnClickListener {
-
         val timePicker = MaterialTimePicker.Builder()
             .setTimeFormat(TimeFormat.CLOCK_24H)
             .setHour(cal.get(Calendar.HOUR))
             .setMinute(cal.get(Calendar.MINUTE))
             .setInputMode(INPUT_MODE_CLOCK)
-            .setTitleText("약속 시간을 설정하세요.")
+            .setTitleText(getString(R.string.title_timepicker))
             .build()
 
-        timePicker.show(supportFragmentManager, "tag")
+        timePicker.show(supportFragmentManager, TIMEPICKER_TAG)
         timePicker.addOnPositiveButtonClickListener {
+            cal.set(Calendar.HOUR, timePicker.hour)
+            cal.set(Calendar.MINUTE, timePicker.minute)
             binding.buttonAddPromiseSelectTime.text =
-                "%d:%d".format(timePicker.hour, timePicker.minute)
+                getString(R.string.time_format).format(
+                    cal.get(Calendar.HOUR),
+                    cal.get(Calendar.MINUTE)
+                )
         }
+    }
+
+    companion object {
+        const val DATEPICKER_TAG = "New Selected Date"
+        const val TIMEPICKER_TAG = "New Selected Time"
     }
 }
