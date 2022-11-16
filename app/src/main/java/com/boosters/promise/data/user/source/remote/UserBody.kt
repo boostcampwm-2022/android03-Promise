523d5d2c9d6bd76ba6d8eec2ea7dd376edc2f2da
dogeby <dogeby@gmail.com>
2022-11-16 16:48:29 +0900

    feat: User 추가
    #7

--- app/src/main/java/com/boosters/promise/data/user/source/remote/UserBody.kt
+++ app/src/main/java/com/boosters/promise/data/user/source/remote/UserBody.kt
@@ -1,9 +1,17 @@
 package com.boosters.promise.data.user.source.remote
 
 import com.boosters.promise.data.user.Location
+import com.boosters.promise.data.user.User
 
 data class UserBody(
     val userCode: String? = null,
     val userName: String? = null,
     val location: Location? = null
 )
+
+fun UserBody.toUser() =
+    User(
+        userCode = userCode ?: throw NullPointerException(),
+        userName = userName ?: "",
+        location = location
+    )
