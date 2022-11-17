296ed7fbc79d69a655eeefcaeaa7bd2512f0c611
dogeby <dogeby@gmail.com>
2022-11-17 14:09:16 +0900

    refactor: User 모듈들을 한 모듈로 모으기
    #7

--- app/src/main/java/com/boosters/promise/data/user/di/UserModule.kt
+++ app/src/main/java/com/boosters/promise/data/user/di/UserModule.kt
@@ -3,6 +3,9 @@ package com.boosters.promise.data.user.di
 import com.boosters.promise.data.user.UserRepository
 import com.boosters.promise.data.user.UserRepositoryImpl
 import com.boosters.promise.data.user.source.remote.UserRemoteDataSource
+import com.boosters.promise.data.user.source.remote.UserRemoteDataSourceImpl
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -11,7 +14,14 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object UserRepositoryModule {
+object UserModule {
+
+    private const val USERS_PATH = "users"
+
+    @Singleton
+    @Provides
+    fun provideUserRemoteDataSource(): UserRemoteDataSource =
+        UserRemoteDataSourceImpl(Firebase.firestore.collection(USERS_PATH))
 
     @Singleton
     @Provides
