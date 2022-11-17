6ae27673d1de3720c9ad3da991ed57a1f48ea9a6
dogeby <dogeby@gmail.com>
2022-11-17 22:12:41 +0900

    feat: User toUserUi 추가
    #35
    Co-authored-by: hoho-97 <cksgh5652@gmail.com>

--- app/src/main/java/com/boosters/promise/data/user/User.kt
+++ app/src/main/java/com/boosters/promise/data/user/User.kt
@@ -1,7 +1,15 @@
 package com.boosters.promise.data.user
 
+import com.boosters.promise.ui.invite.model.UserUiState
+
 data class User(
     val userCode: String,
     val userName: String,
     val location: Location? = null
 )
+
+fun User.toUserUiState() =
+    UserUiState(
+        userName = userName,
+        userCode = userCode
+    )
