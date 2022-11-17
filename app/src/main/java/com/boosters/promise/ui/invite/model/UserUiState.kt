2207b8ed1bbaa80f9788517e2a03f7cf685b051c
dogeby <dogeby@gmail.com>
2022-11-17 22:12:00 +0900

    feat: UserUiState Parceable로 수정, toUser 추가
    #35
    Co-authored-by: hoho-97 <cksgh5652@gmail.com>

--- app/src/main/java/com/boosters/promise/ui/invite/model/UserUiState.kt
+++ app/src/main/java/com/boosters/promise/ui/invite/model/UserUiState.kt
@@ -1,6 +1,17 @@
 package com.boosters.promise.ui.invite.model
 
+import android.os.Parcelable
+import com.boosters.promise.data.user.User
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class UserUiState(
     val userName: String,
     val userCode: String
-)
+) : Parcelable
+
+fun UserUiState.toUser() =
+    User(
+        userName = userName,
+        userCode = userCode
+    )
