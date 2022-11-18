fcecf1a08906139b94b7c1ebca8e2b8fc67c3554
dogeby <dogeby@gmail.com>
2022-11-18 16:46:11 +0900

    refactor: SignUpUiState에서 이름 검증 상태 분리하기
    #42

--- app/src/main/java/com/boosters/promise/ui/signup/SignUpViewModel.kt
+++ app/src/main/java/com/boosters/promise/ui/signup/SignUpViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.boosters.promise.R
 import com.boosters.promise.data.user.UserRepository
+import com.boosters.promise.ui.signup.model.NameInputUiState
 import com.boosters.promise.ui.signup.model.SignUpUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -24,14 +25,19 @@ class SignUpViewModel @Inject constructor(
     private val _isCompleteSignUp = MutableLiveData(false)
     val isCompleteSignUp: LiveData<Boolean> = _isCompleteSignUp
 
+    private val _nameInputUiState = MutableLiveData<NameInputUiState>()
+    val nameInputUiState: LiveData<NameInputUiState> = _nameInputUiState
+
     fun requestSignUp() {
         enterName.value?.let { name ->
             if (nameValidationRegex.matches(name).not()) {
-                _signUpUiState.value = SignUpUiState(
+                _nameInputUiState.value = NameInputUiState(
                     isNameValidationFail = true,
-                    errorText = R.string.signUp_inputError
+                    nameValidationErrorTextResId = R.string.signUp_inputError
                 )
                 return
+            } else {
+                _nameInputUiState.value = NameInputUiState()
             }
             _signUpUiState.value = SignUpUiState(true)
             viewModelScope.launch {
--- app/src/main/java/com/boosters/promise/ui/signup/model/SignUpUiState.kt
+++ app/src/main/java/com/boosters/promise/ui/signup/model/SignUpUiState.kt
@@ -1,9 +1,5 @@
 package com.boosters.promise.ui.signup.model
 
-import androidx.annotation.StringRes
-
 data class SignUpUiState(
-    val isRegistering: Boolean = false,
-    val isNameValidationFail: Boolean = false,
-    @StringRes val errorText: Int? = null
+    val isRegistering: Boolean = false
 )
