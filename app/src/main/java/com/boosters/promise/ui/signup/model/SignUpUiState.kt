a923f4b51b2dc74d97a43e0eccb54cf57eaee4f5
dogeby <dogeby@gmail.com>
2022-11-20 18:29:32 +0900

    style: SignUpUiState 위아래 줄바꿈 적용
    #49

--- app/src/main/java/com/boosters/promise/ui/signup/model/SignUpUiState.kt
+++ app/src/main/java/com/boosters/promise/ui/signup/model/SignUpUiState.kt
@@ -3,7 +3,9 @@ package com.boosters.promise.ui.signup.model
 import androidx.annotation.StringRes
 
 sealed interface SignUpUiState {
+
     object Loading : SignUpUiState
     object Success : SignUpUiState
     class Fail(@StringRes val signUpErrorMessageResId: Int? = null) : SignUpUiState
+
 }
