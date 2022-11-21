bdf67ba547b1e1ec3999c9de9c251c409c1749b8
dogeby <dogeby@gmail.com>
2022-11-21 17:45:32 +0900

    refactor: constructor inject 추가, module에서 사용
    #49

--- app/src/main/java/com/boosters/promise/data/user/di/UserModule.kt
+++ app/src/main/java/com/boosters/promise/data/user/di/UserModule.kt
@@ -1,17 +1,19 @@
 package com.boosters.promise.data.user.di
 
 import android.content.Context
+import androidx.datastore.core.DataStore
 import androidx.datastore.core.handlers.ReplaceFileCorruptionHandler
 import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.emptyPreferences
 import androidx.datastore.preferences.preferencesDataStoreFile
-import com.boosters.promise.data.network.NetworkConnectionUtil
 import com.boosters.promise.data.user.UserRepository
 import com.boosters.promise.data.user.UserRepositoryImpl
 import com.boosters.promise.data.user.source.local.MyInfoLocalDataSource
 import com.boosters.promise.data.user.source.local.MyInfoLocalDataSourceImpl
 import com.boosters.promise.data.user.source.remote.UserRemoteDataSource
 import com.boosters.promise.data.user.source.remote.UserRemoteDataSourceImpl
+import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import dagger.Module
@@ -19,6 +21,7 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Qualifier
 import javax.inject.Singleton
 
 @Module
@@ -28,30 +31,40 @@ object UserModule {
     private const val USERS_PATH = "users"
     private const val MY_INFO_PREFERENCES_NAME = "myInfoPreferences"
 
-    @Singleton
-    @Provides
-    fun provideUserRemoteDataSource(networkConnectionUtil: NetworkConnectionUtil): UserRemoteDataSource =
-        UserRemoteDataSourceImpl(Firebase.firestore.collection(USERS_PATH), networkConnectionUtil)
+    @Qualifier
+    @Retention(AnnotationRetention.BINARY)
+    annotation class UserCollectionReference
 
-    @Singleton
-    @Provides
-    fun provideUserRepository(
-        userRemoteDataSource: UserRemoteDataSource,
-        myInfoLocalDataSource: MyInfoLocalDataSource
-    ): UserRepository {
-        return UserRepositoryImpl(userRemoteDataSource, myInfoLocalDataSource)
-    }
+    @Qualifier
+    @Retention(AnnotationRetention.BINARY)
+    annotation class MyInfoPreferencesDataStore
 
+    @UserCollectionReference
     @Singleton
     @Provides
-    fun provideMyInfoLocalDataSource(@ApplicationContext applicationContext: Context): MyInfoLocalDataSource {
-        val myInfoPreferencesDataStore = PreferenceDataStoreFactory.create(
+    fun provideUserCollectionReference(): CollectionReference = Firebase.firestore.collection(USERS_PATH)
+
+    @MyInfoPreferencesDataStore
+    @Singleton
+    @Provides
+    fun provideMyInfoPreferencesDataStore(@ApplicationContext applicationContext: Context): DataStore<Preferences> {
+        return PreferenceDataStoreFactory.create(
             corruptionHandler = ReplaceFileCorruptionHandler {
                 emptyPreferences()
             },
             produceFile = { applicationContext.preferencesDataStoreFile(MY_INFO_PREFERENCES_NAME) }
         )
-        return MyInfoLocalDataSourceImpl(myInfoPreferencesDataStore)
     }
+
+    @Singleton
+    @Provides
+    fun provideUserRemoteDataSource(userRemoteDataSourceImpl: UserRemoteDataSourceImpl): UserRemoteDataSource = userRemoteDataSourceImpl
+
+    @Singleton
+    @Provides
+    fun provideUserRepository(userRepositoryImpl: UserRepositoryImpl): UserRepository = userRepositoryImpl
+    @Singleton
+    @Provides
+    fun provideMyInfoLocalDataSource(myInfoLocalDataSourceImpl: MyInfoLocalDataSourceImpl): MyInfoLocalDataSource = myInfoLocalDataSourceImpl
 
 }
--- app/src/main/java/com/boosters/promise/data/user/source/remote/UserRemoteDataSourceImpl.kt
+++ app/src/main/java/com/boosters/promise/data/user/source/remote/UserRemoteDataSourceImpl.kt
@@ -2,14 +2,18 @@ package com.boosters.promise.data.user.source.remote
 
 import com.boosters.promise.data.network.NetworkConnectionUtil
 import com.boosters.promise.data.user.User
+import com.boosters.promise.data.user.di.UserModule
 import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.ktx.snapshots
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.mapNotNull
 import kotlinx.coroutines.tasks.await
+import javax.inject.Inject
+import javax.inject.Singleton
 
-class UserRemoteDataSourceImpl(
-    private val userCollectionReference: CollectionReference,
+@Singleton
+class UserRemoteDataSourceImpl @Inject constructor(
+    @UserModule.UserCollectionReference private val userCollectionReference: CollectionReference,
     private val networkConnectionUtil: NetworkConnectionUtil
 ) : UserRemoteDataSource {
 
