b93d37f11f273db572bb3aba954c8f0a4bef2d1c
류대현 <eogus1027@naver.com>
2022-11-23 22:46:55 +0900

    feat: 약속 장소의 위치를 지도에 표시
    #71

--- app/src/main/java/com/boosters/promise/ui/detail/PromiseDetailActivity.kt
+++ app/src/main/java/com/boosters/promise/ui/detail/PromiseDetailActivity.kt
@@ -4,12 +4,21 @@ import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.annotation.UiThread
 import androidx.databinding.DataBindingUtil
 import com.boosters.promise.R
+import com.boosters.promise.data.model.Location
 import com.boosters.promise.databinding.ActivityPromiseDetailBinding
 import com.boosters.promise.ui.promisesetting.model.PromiseUiState
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.geometry.Tm128
+import com.naver.maps.map.CameraUpdate
+import com.naver.maps.map.MapFragment
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.overlay.Marker
 
-class PromiseDetailActivity : AppCompatActivity() {
+class PromiseDetailActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var binding: ActivityPromiseDetailBinding
     private val promiseDetailViewModel: PromiseDetailViewModel by viewModels()
@@ -19,6 +28,22 @@ class PromiseDetailActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_promise_detail)
         setBinding()
         setPromiseInfo()
+        initMap()
+    }
+
+    @UiThread
+    override fun onMapReady(map: NaverMap) {
+        val location = requireNotNull(promiseDetailViewModel.promiseInfo.value).destinationLocation
+        val destinationLocation = convertLocation(location)
+
+        val cameraUpdate = CameraUpdate.scrollTo(destinationLocation)
+        map.moveCamera(cameraUpdate)
+
+        val marker = Marker()
+        marker.apply {
+            position = destinationLocation
+            this.map = map
+        }
     }
 
     private fun setBinding() {
@@ -36,6 +61,20 @@ class PromiseDetailActivity : AppCompatActivity() {
         if (promise != null) {
             promiseDetailViewModel.setPromiseInfo(promise)
         }
+    }
+
+    private fun initMap() {
+        val fm = supportFragmentManager
+        val mapFragment = fm.findFragmentById(R.id.fragment_promiseDetail_map) as MapFragment?
+            ?: MapFragment.newInstance().also {
+                fm.beginTransaction().add(R.id.fragment_promiseDetail_map, it).commit()
+            }
+        mapFragment.getMapAsync(this)
+    }
+
+    private fun convertLocation(location: Location): LatLng {
+        val tm128Location = Tm128(location.x.toDouble(), location.y.toDouble())
+        return tm128Location.toLatLng()
     }
 
     companion object {
