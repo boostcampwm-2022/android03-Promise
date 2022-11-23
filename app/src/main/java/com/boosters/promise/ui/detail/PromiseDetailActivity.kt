9194f8c1eb91b9bf829e08f042f037204de9bf4d
류대현 <eogus1027@naver.com>
2022-11-23 21:08:38 +0900

    design: 약속 상세 화면 레이아웃 구성
    #70

---
+++ app/src/main/java/com/boosters/promise/ui/detail/PromiseDetailActivity.kt
@@ -0,0 +1,14 @@
+package com.boosters.promise.ui.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.boosters.promise.R
+
+class PromiseDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_promise_detail)
+    }
+
+}
