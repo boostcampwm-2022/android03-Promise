eb9a438efe471df770d7bf4856ec9e39df1f711b
Yoo-sumi <hugbee@naver.com>
2022-11-24 13:12:15 +0900

    refactor: 코드 리뷰 반영 #63

--- app/src/main/java/com/boosters/promise/ui/notification/NotificationService.kt
+++ app/src/main/java/com/boosters/promise/ui/notification/NotificationService.kt
@@ -6,10 +6,10 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import android.os.Build
 import androidx.core.app.NotificationCompat
 import com.boosters.promise.R
 import com.boosters.promise.ui.promisesetting.PromiseSettingActivity
+import com.boosters.promise.ui.promisesetting.PromiseSettingViewModel
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
@@ -36,37 +36,23 @@ class NotificationService : FirebaseMessagingService() {
         val notificationManager =
             getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val channel = NotificationChannel(
-                CHANNEL_ID,
-                CHANNEL_NAME,
-                NotificationManager.IMPORTANCE_HIGH
-            ).apply {
-                description = CHANNEL_NAME
-            }
-            notificationManager.createNotificationChannel(channel)
+        val channel = NotificationChannel(
+            CHANNEL_ID,
+            CHANNEL_NAME,
+            NotificationManager.IMPORTANCE_HIGH
+        ).apply {
+            description = CHANNEL_NAME
         }
+        notificationManager.createNotificationChannel(channel)
 
-        val builder = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val notificationBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
-                .setSmallIcon(R.mipmap.ic_launcher)
-                .setContentTitle(remoteMessage.data[MESSAGE_TITLE].toString())
-                .setContentText(remoteMessage.data[MESSAGE_BODY].toString())
-                .setAutoCancel(true)
-                .setContentIntent(pendingIntent)
-                .setCategory(Notification.CATEGORY_MESSAGE)
-                .setPriority(NotificationCompat.PRIORITY_HIGH)
-            notificationBuilder
-        } else {
-            val notificationBuilder = NotificationCompat.Builder(this)
-                .setSmallIcon(R.mipmap.ic_launcher)
-                .setContentTitle(remoteMessage.data[MESSAGE_TITLE].toString())
-                .setContentText(remoteMessage.data[MESSAGE_BODY].toString())
-                .setAutoCancel(true)
-                .setContentIntent(pendingIntent)
-                .setCategory(Notification.CATEGORY_MESSAGE)
-            notificationBuilder
-        }
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setContentTitle(remoteMessage.data[MESSAGE_TITLE].toString())
+            .setContentText(String.format(getString(R.string.promiseSetting_notification), remoteMessage.data[MESSAGE_BODY].toString()))
+            .setAutoCancel(true)
+            .setContentIntent(pendingIntent)
+            .setCategory(Notification.CATEGORY_MESSAGE)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
 
         notificationManager.notify(uniId, builder.build())
     }
