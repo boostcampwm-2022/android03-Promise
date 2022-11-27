150b3d2e02e13cb0ce91c710c4584d17edbe0102
dogeby <dogeby@gmail.com>
2022-11-27 17:52:12 +0900

    refactor: PromiseBody User가 아니라 userCode를 저장하도록 수정
    비동기작업으로인해 #110 버그 존재
    
    #106

--- app/src/main/java/com/boosters/promise/data/promise/PromiseRepositoryImpl.kt
+++ app/src/main/java/com/boosters/promise/data/promise/PromiseRepositoryImpl.kt
@@ -3,12 +3,16 @@ package com.boosters.promise.data.promise
 import com.boosters.promise.data.promise.source.remote.PromiseRemoteDataSource
 import com.boosters.promise.data.promise.source.remote.toPromise
 import com.boosters.promise.data.user.User
+import com.boosters.promise.data.user.UserRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.mapNotNull
 import javax.inject.Inject
 
 class PromiseRepositoryImpl @Inject constructor(
     private val promiseRemoteDataSource: PromiseRemoteDataSource,
+    private val userRepository: UserRepository
 ) : PromiseRepository {
 
     override fun addPromise(promise: Promise): Flow<Boolean> {
@@ -20,15 +24,19 @@ class PromiseRepositoryImpl @Inject constructor(
     }
 
     override fun getPromise(promiseId: String): Flow<Promise> {
-        return promiseRemoteDataSource.getPromise(promiseId).mapNotNull { it.toPromise() }
+        return promiseRemoteDataSource.getPromise(promiseId).mapNotNull { promiseBody ->
+            promiseBody.toPromise(userRepository.getUserList(promiseBody.members).first())
+        }
     }
 
     override fun getPromiseList(user: User, date: String): Flow<List<Promise>> {
-        return promiseRemoteDataSource.getPromiseList(user, date).mapNotNull { promiseList ->
-            try {
-                promiseList.map { it.toPromise() }
-            } catch (e: NullPointerException) {
-                null
+        return promiseRemoteDataSource.getPromiseList(user, date).map { promiseList ->
+            promiseList.mapNotNull { promiseBody ->
+                try {
+                    promiseBody.toPromise(userRepository.getUserList(promiseBody.members).first())
+                } catch (e: NullPointerException) {
+                    null
+                }
             }
         }
     }
--- app/src/main/java/com/boosters/promise/data/promise/source/remote/PromiseRemoteDataSourceImpl.kt
+++ app/src/main/java/com/boosters/promise/data/promise/source/remote/PromiseRemoteDataSourceImpl.kt
@@ -7,7 +7,6 @@ import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.flow.mapNotNull
-import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
 
 class PromiseRemoteDataSourceImpl @Inject constructor(
