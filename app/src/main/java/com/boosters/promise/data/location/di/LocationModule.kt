d359842d672db62a383cb9e12c8d06f6fe3542e3
dogeby <dogeby@gmail.com>
2022-11-29 02:56:26 +0900

    feat: 사용자 위치정보 RemoteDataSource 구현
    #106

--- app/src/main/java/com/boosters/promise/data/location/di/LocationModule.kt
+++ app/src/main/java/com/boosters/promise/data/location/di/LocationModule.kt
@@ -5,16 +5,31 @@ import com.boosters.promise.data.location.LocationRepository
 import com.boosters.promise.data.location.LocationRepositoryImpl
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices.getFusedLocationProviderClient
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Qualifier
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object LocationModule {
+
+    private const val LOCATIONS_PATH = "locations"
+
+    @Qualifier
+    @Retention(AnnotationRetention.BINARY)
+    annotation class LocationCollectionReference
+
+    @LocationCollectionReference
+    @Singleton
+    @Provides
+    fun provideLocationReference(): CollectionReference = Firebase.firestore.collection(LOCATIONS_PATH)
 
     @Provides
     @Singleton
