1997150d9bafd3dbaea36e064f2810ee41bb1010
박찬호 <cksgh5652@gmail.com>
2022-11-30 11:25:02 +0900

    feat: 약속 목록 출력 시간순 정렬
    #121

--- app/src/main/java/com/boosters/promise/ui/promisecalendar/PromiseCalendarViewModel.kt
+++ app/src/main/java/com/boosters/promise/ui/promisecalendar/PromiseCalendarViewModel.kt
@@ -1,5 +1,6 @@
 package com.boosters.promise.ui.promisecalendar
 
+import android.icu.text.SimpleDateFormat
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.boosters.promise.data.promise.Promise
@@ -10,6 +11,7 @@ import com.boosters.promise.ui.promisecalendar.model.UserUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
+import java.util.*
 import javax.inject.Inject
 
 @HiltViewModel
@@ -28,6 +30,8 @@ class PromiseCalendarViewModel @Inject constructor(
 
     private val _dailyPromiseList = MutableStateFlow(emptyList<Promise>())
     val dailyPromiseList: StateFlow<List<Promise>> get() = _dailyPromiseList.asStateFlow()
+
+    private val dateFormatter = SimpleDateFormat(DATE_FORMAT, Locale.getDefault())
 
     init {
         loadMyInfo()
@@ -64,10 +68,16 @@ class PromiseCalendarViewModel @Inject constructor(
                 if (it is PromiseListUiState.Success) {
                     _dailyPromiseList.value = it.data.filter { promise ->
                         promise.date == date
+                    }.sortedBy { promise ->
+                        dateFormatter.parse(promise.time).time
                     }
                 }
             }
         }
+    }
+
+    companion object {
+        private const val DATE_FORMAT = "HH:mm"
     }
 
 }
